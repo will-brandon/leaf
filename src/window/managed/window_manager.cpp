32fb3637867b087f7187397e475ca36411279557
wbrandon <will17brandon@icloud.com>
2023-05-07 11:30:15 -0400

    Implementing a window manager

--- src/window/managed/window_manager.cpp
+++ src/window/managed/window_manager.cpp
@@ -12,7 +12,32 @@
 
 using namespace std;
 
+#include "window_manager.hpp"
+
 namespace leaf
 {
+    size_t window_manager::window_count(void) const noexcept
+    {
+        // Count the number of windows in the set of windows.
+        return m_windows.size();
+    }
 
+    size_t window_manager::living_window_count(void) const noexcept
+    {
+        // Initialize a window counter.
+        size_t count = 0;
+
+        // For each window under management, check if it is alive.
+        for (const managed_window *window : m_windows)
+        {
+            // If the window is alive, increment the counter.
+            if (window->is_alive())
+            {
+                count++;
+            }
+        }
+
+        // Return the counter.
+        return count;
+    }
 }
--- src/window/managed/window_manager.hpp
+++ src/window/managed/window_manager.hpp
@@ -38,35 +38,14 @@ namespace leaf
              * 
              * @return  the number of windows being managed
              */
-            inline size_t window_count(void) const noexcept
-            {
-                // Count the number of windows in the set of windows.
-                return m_windows.size();
-            }
+            size_t window_count(void) const noexcept;
 
             /**
              * @brief   Determines how many managed windows are alive (not closed).
              * 
              * @return  the number of living managed windows
              */
-            inline size_t living_window_count(void) const noexcept
-            {
-                // Initialize a window counter.
-                size_t count = 0;
-                
-                // For each window under management, check if it is alive.
-                for (const managed_window *window : m_windows)
-                {
-                    // If the window is alive, increment the counter.
-                    if (window->is_alive())
-                    {
-                        count++;
-                    }
-                }
-
-                // Return the counter.
-                return count;
-            }
+            size_t living_window_count(void) const noexcept;
     };
 }
 
