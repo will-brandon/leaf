a22814bcdc3096c1c356520db4fdf0e1b97119a2
wbrandon <will17brandon@icloud.com>
2023-05-09 16:59:41 -0400

    Added includes and redid header guards

---
+++ include/leaf/leaf.hpp
@@ -0,0 +1,16 @@
+/// 
+/// @file       leaf.hpp
+/// @author     Will Brandon (brandon.w@northeastern.edu)
+/// @date       May 9, 2023
+/// 
+/// @brief      Header to include the entire Leaf GUI library.
+/// 
+/// @copyright  Copyright (c) 2023
+/// 
+
+#ifndef LEAF_INCLUDE_LEAF_HEADER_GUARD
+#define LEAF_INCLUDE_LEAF_HEADER_GUARD
+
+
+
+#endif
---
+++ include/leaf/window.hpp
@@ -0,0 +1,16 @@
+/// 
+/// @file       window.hpp
+/// @author     Will Brandon (brandon.w@northeastern.edu)
+/// @date       May 9, 2023
+/// 
+/// @brief      Header to include the window portion of the Leaf GUI library.
+/// 
+/// @copyright  Copyright (c) 2023
+/// 
+
+#ifndef LEAF_INCLUDE_WINDOWS_HEADER_GUARD
+#define LEAF_INCLUDE_WINDOWS_HEADER_GUARD
+
+
+
+#endif
